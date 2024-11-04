df430b9a42cff5eeea1a197def090f2a5d18ee1a
power-minu <mw0677@naver.com>
2024-11-04 21:29:28 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/mutsa/yewon/talksparkbe/TalkSparkBeApplication.java
@@ -0,0 +1,13 @@
+package mutsa.yewon.talksparkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkSparkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkSparkBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mutsa/yewon/talksparkbe/TalkSparkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mutsa.yewon.talksparkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkSparkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
