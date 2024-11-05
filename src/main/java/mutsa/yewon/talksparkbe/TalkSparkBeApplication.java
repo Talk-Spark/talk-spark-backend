094ccd70a4568c00424367c58ae7fdf05115a0bf
power-minu <mw0677@naver.com>
2024-11-05 11:03:36 +0900

    Feat: #1 OOO 기능을 위한 작업 1 수행

--- src/main/java/mutsa/yewon/talksparkbe/TalkSparkBeApplication.java
+++ src/main/java/mutsa/yewon/talksparkbe/TalkSparkBeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TalkSparkBeApplication {
 
+    // 이번 이슈에 해당하는 작업을 진행합니다.
     public static void main(String[] args) {
         SpringApplication.run(TalkSparkBeApplication.class, args);
     }
