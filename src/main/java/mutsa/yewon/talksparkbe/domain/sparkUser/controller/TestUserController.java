57dc6e1f1de3444a0ea1cfbc6f989bce2e48e6cb
power-minu <mw0677@naver.com>
2024-12-03 19:26:19 +0900

    feat: #19 테스트 편의를 위해 테스트 유저 삽입 기능 구현, 액세스토큰 유효기간 12시간으로 설정

--- src/main/java/mutsa/yewon/talksparkbe/domain/sparkUser/controller/TestUserController.java
+++ src/main/java/mutsa/yewon/talksparkbe/domain/sparkUser/controller/TestUserController.java
@@ -1,30 +1,27 @@
 package mutsa.yewon.talksparkbe.domain.sparkUser.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import mutsa.yewon.talksparkbe.domain.sparkUser.SparkUserControllerDocs;
 import mutsa.yewon.talksparkbe.domain.sparkUser.dto.SparkUserDTO;
 import mutsa.yewon.talksparkbe.domain.sparkUser.service.SparkUserService;
 import mutsa.yewon.talksparkbe.global.util.JWTUtil;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@Log4j2
-public class SparkUserController implements SparkUserControllerDocs {
+public class TestUserController {
 
     private final SparkUserService sparkUserService;
     private final JWTUtil jwtUtil;
 
-    @PostMapping("/api/member/kakao")
-    public Map<String, Object> login(String accessToken) {
-        SparkUserDTO kakaoUser = sparkUserService.getKakaoUser(accessToken);
+    @PostMapping("/api/test-user")
+    public Map<String, Object> login(@RequestBody String name) {
+        SparkUserDTO testUser = sparkUserService.generateTestUser(name);
 
-        Map<String, Object> claims = kakaoUser.getClaims();
+        Map<String, Object> claims = testUser.getClaims();
 
         String JWTAccessToken = jwtUtil.generateToken(claims, 60 * 12);
         String JWTRefreshToken = jwtUtil.generateToken(claims, 60 * 24);
@@ -35,12 +32,4 @@ public class SparkUserController implements SparkUserControllerDocs {
         return claims;
     }
 
-    @GetMapping("/api/member/refresh")
-    public Map<String, Object> refresh(String refreshToken) {
-        Map<String, Object> claims = jwtUtil.validateToken(refreshToken);
-
-        String accessToken = jwtUtil.generateToken(claims, 10);
-
-        return Map.of("accessToken", accessToken, "refreshToken", refreshToken);
-    }
 }
