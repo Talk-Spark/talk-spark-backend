3e49bd011e02ba6410b64be1498824f5cde088c4
power-minu <mw0677@naver.com>
2024-12-08 18:00:37 +0900

    레디스 호스트 설정

--- src/main/java/mutsa/yewon/talksparkbe/domain/game/config/RedissonConfig.java
+++ src/main/java/mutsa/yewon/talksparkbe/domain/game/config/RedissonConfig.java
@@ -3,18 +3,22 @@ package mutsa.yewon.talksparkbe.domain.game.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${redis.host}")
+    private String redisHost;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         // 단일 노드 Redis 서버 주소 설정
         config.useSingleServer()
-                .setAddress("redis://127.0.0.1:6379") // Redis 서버 주소와 포트
+                .setAddress("redis://" + redisHost + ":6379") // Redis 서버 주소와 포트
                 .setConnectionPoolSize(10) // 연결 풀 크기 설정
                 .setConnectionMinimumIdleSize(5); // 최소 유휴 연결 수 설정
 
